75ad0a4a6ddf44153fcc101cef100a2afb09db32
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-16 20:54:25 +0100

    Add typescript type checking

--- practices/practice2/put.ts
+++ practices/practice2/put.ts
@@ -1,23 +1,23 @@
 import { DynamoDB } from "aws-sdk";
+import { Context } from "aws-lambda";
 
-const handler = async (event: any, _context: any) => {
-    // Already configured by AWS
-    const client = new DynamoDB.DocumentClient()
-    var params = {
-        // Insertam el que vagis a ficar dintre de l'item dins d'aquesta taula
-        TableName: "pablo-fraile-dynamodb-table",
-        Item: {
-            // Tres punts -> ens permet no haver de fer per cada un dels events.data
-            // Desmonta'm el objecte data i desmonta'l aqui
-            // id: 1
-            // data: {
-            //     key1: value1
-            //     key2: value2
-            // }
-            id: event.id, ...event.data
-        }
-    }
-    await client.put(params).promise()
+type ID = string | number;
+
+interface PutRequest {
+  id: ID;
+  data: object;
 }
 
-export {handler}
+const handler = async (event: PutRequest, _context: Context) => {
+  const client = new DynamoDB.DocumentClient();
+  const params = {
+    TableName: "pablo-fraile-dynamodb-table",
+    Item: {
+      id: event.id,
+      ...event.data,
+    },
+  };
+  await client.put(params).promise();
+};
+
+export { handler };

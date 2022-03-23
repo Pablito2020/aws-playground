5a062544b716b49582f8fc6472f43ac139c4ef25
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-23 22:55:11 +0100

    Practice 3: Implemented Player 1

--- practices/practice3/player1.ts
+++ practices/practice3/player1.ts
@@ -1,10 +1,50 @@
 import {
-    Context,
+    Context, EventBridgeEvent,
 } from 'aws-lambda';
-const handler = async (event: any, context: Context) : Promise<void> => {
-    console.log(event)
-    console.log(context.functionName)
-    console.log('Hello world')
+import {EventBridge} from "aws-sdk";
+
+interface Match {
+    round: string
+}
+
+const LATEST_ROUND = 9
+
+const handler = async (event: EventBridgeEvent<string, Match>, _context: Context): Promise<void> => {
+    const round = Number.parseInt(event.detail.round) + 1
+    if (round > LATEST_ROUND)
+        console.log(`Error: Rounds are higher than ${LATEST_ROUND}`)
+    else if (!playerCanReturnBall()) {
+        console.log(`Error: Couldn't return the ball`)
+    } else {
+        console.log("Returning the ball")
+        const data = await sendPlayerEvent(round)
+        console.log(data)
+    }
+}
+
+function playerCanReturnBall():Boolean {
+    const number = getRandomInt(0, 10)
+    console.log(`Random number is: ${number}`)
+    return number <= 7
+}
+
+function getRandomInt(min: number, max:number) : number{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sendPlayerEvent(round: number): Promise<EventBridge.PutEventsResponse> {
+    const eventBridge = new EventBridge()
+    const detail = {round: round.toString()}
+    const params = {
+        Entries: [{
+            Source: "player1",
+            Detail: JSON.stringify(detail),
+            DetailType: "ping-pong-event",
+        },]
+    }
+    return eventBridge.putEvents(params).promise()
 }
 
 export {handler}

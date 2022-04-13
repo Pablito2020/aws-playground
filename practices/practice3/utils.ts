57d2bd74221035d4fac2bc900a7c4ddef55ace5f
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-13 18:43:29 +0200

    add xray

--- practices/practice3/utils.ts
+++ practices/practice3/utils.ts
@@ -1,5 +1,6 @@
 import {EventBridgeEvent} from "aws-lambda";
-import {EventBridge} from "aws-sdk";
+const AWSXRay = require('aws-xray-sdk-core');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
 import {getRound} from "./RoundGetter";
 import {playerCanReturnBall} from "./BallReturner";
 
@@ -12,19 +13,30 @@ const LATEST_ROUND = 9
 export async function playerMove(player: string, event: EventBridgeEvent<string, Match>) {
     const round = getRound(event.detail.round)
     console.log(`Current round is: ${round}`)
-    if (round > LATEST_ROUND)
+    if (round > LATEST_ROUND) {
+        const segment = AWSXRay.getSegment();
+        const subsegment = segment.addNewSubsegment('roundIsOutOfRange');
+        subsegment.close();
         console.log(`Error: Rounds are higher than ${LATEST_ROUND}`)
-    else if (!playerCanReturnBall()) {
+    } else if (!playerCanReturnBall()) {
+        const segment = AWSXRay.getSegment();
+        const subsegment = segment.addNewSubsegment('playerCanNotReturnBall');
+        subsegment.close();
         console.log(`Error: Couldn't return the ball`)
     } else {
         console.log("Returning the ball")
+        const segment = AWSXRay.getSegment();
+        const subsegment = segment.addNewSubsegment('putEventInEventBridge');
+        subsegment.addAnnotation("round", round);
+        subsegment.addAnnotation("player", player);
         const data = await sendPlayerEvent(round, player)
+        subsegment.close();
         console.log(data)
     }
 }
 
-function sendPlayerEvent(round: number, player: string): Promise<EventBridge.PutEventsResponse> {
-    const eventBridge = new EventBridge()
+function sendPlayerEvent(round: number, player: string): Promise<AWS.EventBridge.PutEventsResponse> {
+    const eventBridge = new AWS.EventBridge()
     const detail = {round: round.toString()}
     const params = {
         Entries: [
